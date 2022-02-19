0c9f601ba02c691432415f0d7890fbac0274d5e9
Addict07 <addictedchrysler@gmail.com>
2022-02-20 00:30:20 +0800

    add signin function to api utlity class

--- lib/util/api.dart
+++ lib/util/api.dart
@@ -1,11 +1,19 @@
 import 'dart:convert';
 
 import 'package:dio/dio.dart';
+import 'package:http/http.dart' as http;
+import 'package:logger/logger.dart';
 import 'package:studentprojectmanager/models/category.dart';
 import 'package:xml2json/xml2json.dart';
 
 class Api {
   Dio dio = Dio();
+
+  // static String apiUrl = 'http://192.168.137.1:3000/api/v1';
+  static String apiUrl = 'http://192.168.137.1:3000/api/v1';
+  static String registerEndPoint = '$apiUrl/users/register';
+  static String loginEndPoint = '$apiUrl/users/login';
+
   static String baseURL = 'https://catalog.feedbooks.com';
   static String publicDomainURL = '$baseURL/publicdomain/browse';
   static String popular = '$publicDomainURL/top.atom';
@@ -18,6 +26,8 @@ class Api {
   static String mystery = '$publicDomainURL/top.atom?cat=FBFIC022000';
   static String romance = '$publicDomainURL/top.atom?cat=FBFIC027000';
   static String horror = '$publicDomainURL/top.atom?cat=FBFIC015000';
+
+  var logger = Logger();
 
   Future<CategoryFeed> getCategory(String url) async {
     var res = await dio.get(url).catchError((e) {
@@ -34,5 +44,24 @@ class Api {
       throw ('Error ${res.statusCode}');
     }
     return category;
+  }
+
+  Future<Map<String, dynamic>?> signIn(Map<String, String> body) async {
+    // var uri = Uri.parse(loginEndPoint);
+    // var response = await http.post(uri, body: body);
+    var response = await dio.post(loginEndPoint, data: body);
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      // logger.i( response.data);
+      // logger.i(data);
+      logger.d(response.data?['user'], response.data?['token'].toString());
+      return response.data;
+    }
+
+    return null;
+  }
+
+  Future<Map<String, dynamic>?> register(var body) async {
+    return null;
   }
 }
