3b9d0eac601366d27ac8311bf00b60effd4bc60c
Addict07 <addictedchrysler@gmail.com>
2022-02-25 06:02:47 +0800

    add functions to create and fetch projects

--- lib/util/api.dart
+++ lib/util/api.dart
@@ -1,9 +1,12 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:dio/dio.dart';
 import 'package:http/http.dart' as http;
 import 'package:logger/logger.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:studentprojectmanager/models/category.dart';
+import 'package:http_parser/http_parser.dart';
 import 'package:xml2json/xml2json.dart';
 
 class Api {
@@ -13,6 +16,8 @@ class Api {
   static String apiUrl = 'http://192.168.137.1:3000/api/v1';
   static String registerEndPoint = '$apiUrl/users/register';
   static String loginEndPoint = '$apiUrl/users/login';
+  static String getCategoriesEndPoint = '$apiUrl/categories';
+  static String uploadProjectEndPoint = '$apiUrl/projects';
 
   static String baseURL = 'https://catalog.feedbooks.com';
   static String publicDomainURL = '$baseURL/publicdomain/browse';
@@ -89,6 +94,115 @@ class Api {
     } else {
       res = {};
     }
+    return res;
+  }
+
+  Future<Map<String, dynamic>?> uploadProject(
+      Map<String, String> body, String? filePath, String? imagePath) async {
+    var uri = Uri.parse(uploadProjectEndPoint);
+
+    var sp = await SharedPreferences.getInstance();
+    List<String>? user = await sp.getStringList("user");
+    String token = user![1];
+
+    Map<String, String> headers = {
+      'Authorization': 'Bearer $token',
+    };
+
+    var request = http.MultipartRequest("POST", uri);
+    request.fields.addAll(body);
+    request.headers.addAll(headers);
+
+    if (imagePath != null) {
+      request.files.add(await http.MultipartFile.fromPath("image", imagePath!,
+          contentType: MediaType('image', 'jpg')));
+      // request.files.add(new http.MultipartFile.fromBytes(
+      //     'file', await File.fromUri(Uri.parse(imagePath)).readAsBytes(),
+      //     contentType: new MediaType('image', 'jpeg')));
+    }
+
+    if (filePath != null) {
+      request.files
+          .add(await http.MultipartFile.fromPath('document', filePath!));
+      // request.files.add(new http.MultipartFile.fromBytes(
+      //     'file', await File.fromUri(Uri.parse(filePath)).readAsBytes(),
+      //     contentType: new MediaType('image', 'jpeg')));
+    }
+
+    logger.i([
+      request.files.first,
+      request.files.last,
+    ]);
+
+    var response = await request.send();
+    logger.i([response.stream, response.statusCode]);
+    final res = await http.Response.fromStream(response);
+    // Map<String, dynamic> res;
+
+    logger.d(res);
+
+    // if (response.statusCode == 200 || response.statusCode == 201) {
+    //   // user found
+    //   res = {"message": "success", "body": json.decode(response.body)};
+    // } else if (response.statusCode == 400 || response.statusCode == 404) {
+    //   // user not found
+    //   res = {"message": "failed", "body": response.body};
+    // } else if (response.statusCode >= 500) {
+    //   // server error
+    //   res = {"message": "error", "body": null};
+    // } else {
+    //   res = {};
+    // }
+
+    return null;
+  }
+
+  Future<Map<String, dynamic>?> getCategories() async {
+    Uri uri = Uri.parse(getCategoriesEndPoint);
+    http.Response response =
+        await http.get(uri, headers: {'Content-Type': 'Application/json'});
+
+    Map<String, dynamic> res;
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      // user found
+      res = {"message": "success", "body": json.decode(response.body)};
+    } else if (response.statusCode == 400 || response.statusCode == 404) {
+      // user not found
+      res = {"message": "failed", "body": response.body};
+    } else if (response.statusCode >= 500) {
+      // server error
+      res = {"message": "error", "body": null};
+    } else {
+      res = {};
+    }
+
+    logger.d(res);
+    return res;
+  }
+
+  Future<Map<String, dynamic>?> getProjects() async {
+    Uri uri = Uri.parse(uploadProjectEndPoint);
+    http.Response response =
+        await http.get(uri, headers: {'Content-Type': 'Application/Json'});
+
+    Map<String, dynamic> res;
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      // user found
+      res = {"message": "success", "body": json.decode(response.body)};
+    } else if (response.statusCode == 400 || response.statusCode == 404) {
+      // user not found
+      res = {"message": "failed", "body": response.body};
+    } else if (response.statusCode >= 500) {
+      // server error
+      res = {"message": "error", "body": null};
+    } else {
+      res = {};
+    }
+
+    logger.d(res);
+
     return res;
   }
 }
